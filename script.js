405cf2bfa836dd45525e015d453a4319e15f7f85
EmanzElsayed <emanelsayed2024@gmail.com>
2024-04-26 23:16:30 +0300

    refactoring code

--- script.js
+++ script.js
@@ -17,26 +17,19 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess != secretNumber) {
     score--;
     if (score > 0) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
       document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too High';
-    } else {
+    } else
       document.querySelector('.message').textContent = '💥 You lost the game!';
-    }
-  } else if (guess < secretNumber) {
-    score--;
-    if (score > 0) {
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'Too Low';
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-    }
   }
 });
 
